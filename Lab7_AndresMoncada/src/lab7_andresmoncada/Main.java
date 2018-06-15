ba79cb1c62bcfe3250a57235be5d4621623444cb
Andres Moncada <andresm1996@hotmail.com>
2018-06-15 14:51:47 -0600

    Agregar piezas y eliminarlas, al agregar carros se limpia el text field

--- Lab7_AndresMoncada/src/lab7_andresmoncada/Main.java
+++ Lab7_AndresMoncada/src/lab7_andresmoncada/Main.java
@@ -7,6 +7,9 @@ package lab7_andresmoncada;
 
 import java.util.ArrayList;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 /**
  *
@@ -30,6 +33,8 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        menu_tree = new javax.swing.JPopupMenu();
+        mi_elim = new javax.swing.JMenuItem();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         tf_carro = new javax.swing.JTextField();
@@ -41,11 +46,20 @@ public class Main extends javax.swing.JFrame {
         ensamblar = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         tf_pieza = new javax.swing.JTextField();
-        jSpinner1 = new javax.swing.JSpinner();
+        s_tiempo = new javax.swing.JSpinner();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         addPieza = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+
+        mi_elim.setText("Eliminar pieza");
+        mi_elim.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_elimActionPerformed(evt);
+            }
+        });
+        menu_tree.add(mi_elim);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -56,6 +70,11 @@ public class Main extends javax.swing.JFrame {
 
         javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("Carro");
         tree.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
+        tree.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                treeMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tree);
 
         cb_carros.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Carros" }));
@@ -81,7 +100,7 @@ public class Main extends javax.swing.JFrame {
 
         jLabel4.setText("Nombre de la pieza");
 
-        jSpinner1.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
+        s_tiempo.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
 
         jLabel5.setText("Tiempo de ensamblaje");
 
@@ -92,16 +111,21 @@ public class Main extends javax.swing.JFrame {
 
         addPieza.setText("Agregar");
         addPieza.setEnabled(false);
+        addPieza.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addPiezaActionPerformed(evt);
+            }
+        });
+
+        jLabel8.setForeground(new java.awt.Color(100, 100, 100));
+        jLabel8.setText("Se pueden eliminar piezas dándoles clic derecho en el árbol");
+        jLabel8.setAutoscrolls(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(ensamblar)
-                .addGap(86, 86, 86))
-            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -119,7 +143,7 @@ public class Main extends javax.swing.JFrame {
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(s_tiempo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(jLabel6))
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -139,42 +163,54 @@ public class Main extends javax.swing.JFrame {
                         .addGap(238, 238, 238)))
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(ensamblar)
+                        .addGap(78, 78, 78))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cb_carros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addGap(17, 17, 17)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(tf_carro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(addCarro)
-                .addGap(83, 83, 83)
-                .addComponent(jLabel3)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(tf_pieza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(cb_carros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addGap(17, 17, 17)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(tf_carro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(addCarro)
+                        .addGap(83, 83, 83)
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(tf_pieza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(s_tiempo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(addPieza))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(12, 12, 12)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(addPieza)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(ensamblar)
-                .addGap(0, 35, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -189,6 +225,7 @@ public class Main extends javax.swing.JFrame {
         ensamblar.setEnabled(true);
         addPieza.setEnabled(true);
         tree.setModel(carros.get(carros.size()-1).getModel());
+        tf_carro.setText("");
     }//GEN-LAST:event_addCarroActionPerformed
 
     private void cb_carrosItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cb_carrosItemStateChanged
@@ -199,6 +236,44 @@ public class Main extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_cb_carrosItemStateChanged
+
+    private void treeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_treeMouseClicked
+        int row = tree.getClosestRowForLocation(evt.getX(), evt.getY());
+        tree.setSelectionRow(row);
+        if(evt.isMetaDown() && row > 0){
+            menu_tree.show(tree, evt.getX(), evt.getY());
+        }
+    }//GEN-LAST:event_treeMouseClicked
+
+    private void mi_elimActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_elimActionPerformed
+        try {
+            DefaultTreeModel tm = (DefaultTreeModel) tree.getModel();
+            DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getSelectionPath().getLastPathComponent();
+            tm.removeNodeFromParent(nodo);
+            tm.reload();
+            JOptionPane.showMessageDialog(this, "Pieza elminada");
+        } catch (Exception e) {
+            
+        }
+    }//GEN-LAST:event_mi_elimActionPerformed
+
+    private void addPiezaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addPiezaActionPerformed
+        if(tree.getLeadSelectionRow() >= 0){
+            try{
+                DefaultTreeModel tm = (DefaultTreeModel) tree.getModel();
+                DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getSelectionPath().getLastPathComponent();
+                nodo.add(
+                        new DefaultMutableTreeNode(
+                                new Pieza(tf_pieza.getText(),(int)s_tiempo.getValue())));
+                tm.reload();
+                tf_pieza.setText("");
+                s_tiempo.setValue(1);
+            }catch(Exception e){
+                
+            }
+        }else
+            JOptionPane.showMessageDialog(this, "Seleccione una pieza o carro para\n"+"agregarle esta pieza");
+    }//GEN-LAST:event_addPiezaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -247,8 +322,11 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSpinner jSpinner1;
+    private javax.swing.JPopupMenu menu_tree;
+    private javax.swing.JMenuItem mi_elim;
+    private javax.swing.JSpinner s_tiempo;
     private javax.swing.JTextField tf_carro;
     private javax.swing.JTextField tf_pieza;
     private javax.swing.JTree tree;
